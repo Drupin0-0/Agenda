d6e73fdfc65ee1a4dd8e2c09cebba37e35449d21
Eduardo <linseduardo2009@gmail.com>
2025-05-19 22:38:04 -0300

    urls

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from contact.models import Contact
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
     context = {
         'contacts': contacts,
     }
@@ -10,5 +10,17 @@ def index(request):
         
         request,
         'contact/index.html',
+        context
+    )
+    
+def contact(request, contact_id):
+    single_contact = Contact.objects.get(pk=contact_id)
+    context = {
+        'contact': single_contact,
+    }
+    return render(
+        
+        request,
+        'contact/contact.html',
         context
     )
