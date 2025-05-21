10ece818e920ab8bc6c7a4ebaf006eba42571be0
Eduardo <linseduardo2009@gmail.com>
2025-05-20 21:50:14 -0300

    focus

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,26 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 from contact.models import Contact
-# Create your views here.
+
+
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    single_contact = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[10:20]
+
     context = {
-        'contacts': contacts,
+        'contacts': single_contact,
+        'site_title': 'Contatos -'
     }
     return render(
-        
         request,
         'contact/index.html',
         context
     )
-    
+
+
 def contact(request, contact_id):
-    single_contact = Contact.objects.get(pk=contact_id)
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
     context = {
         'contact': single_contact,
     }
+
     return render(
-        
         request,
         'contact/contact.html',
         context
     )
+    
