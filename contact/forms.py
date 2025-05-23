11f7d79c90f4667d858a0fd1f033981f93c72d66
Eduardo <linseduardo2009@gmail.com>
2025-05-23 18:37:48 -0300

    filter

--- contact/forms.py
+++ contact/forms.py
@@ -20,7 +20,12 @@ class ContactForm(forms.ModelForm):
         })
     class Meta:
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone')
+        fields = (
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category'
+            
+            
+            )
         # widgets = {
         #     'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escreva aqui'}),
         #     'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escreva aqui'}),
