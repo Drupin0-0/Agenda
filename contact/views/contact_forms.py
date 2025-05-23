94cb698c1f76a5574f02b84e3bddf18cb84b063f
Eduardo <linseduardo2009@gmail.com>
2025-05-22 22:54:03 -0300

    create

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,22 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+
+from contact.models import Contact
+from django import forms
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone')
+
+def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+    }
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
